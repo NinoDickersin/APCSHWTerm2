0e1708e3b418b622a752278d582df8c47159a63c
Nino Dickersin <n.dickersin@gmail.com>
2015-02-04 19:10:46 -0500

    fact, fib, sqrt methods written

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,55 @@
+import java.lang.*;
+public class Recursion implements hw1{
+
+    public String name(){
+	return "Dickersin,Nino";
+    }
+
+    public void illegalArg(double n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	} 
+    }
+
+    public int fact(int n){
+	illegalArg(n);
+	if (n < 2){
+	    return 1;
+	}
+	return n * fact(n - 1);
+    }
+
+    public int fib(int n){
+	illegalArg(n);
+	if (n < 1){
+	    return 0;
+	}else if(n < 3){
+	    return 1;
+	}	
+	return fib(n - 1) + fib (n - 2);
+    }
+
+    public double sqrt(double n){
+	illegalArg(n);
+	return guesser (n, n / 2);
+    }
+
+    public double guesser (double n, double guess){
+	if (Math.abs(guess - ((n / guess + guess) / 2)) < 0.00000001){
+	    return guess;
+	}
+	return guesser (n, ((n / guess + guess) / 2));
+    }
+
+    
+public static void main(String[]args){
+    Recursion a = new Recursion();
+    System.out.println(a.name());
+    System.out.println(a.fact(5));
+    System.out.println(a.fib(30));
+    System.out.println(a.sqrt(18742));
+    System.out.println(a.fact(-10));
+    System.out.println(a.fib(10));
+}
+    
+}
