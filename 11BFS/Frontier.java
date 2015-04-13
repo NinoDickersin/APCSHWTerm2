2200ef66fdf800734b0367afe4b9dcf80578c3c7
Nino Dickersin <n.dickersin@gmail.com>
2015-04-12 21:54:30 -0400

    finished MazeSolver (the reason there's only one commit is because I did it over spring break vacation w/ no internet)

--- 11BFS/Frontier.java
+++ 11BFS/Frontier.java
@@ -2,148 +2,30 @@ import java.util.*;
 
 public class Frontier<T>{
 
-    public Object[] queue;
-    public int head;
-    public int tail;
-    public int size;
+    public int type;
+    public MyDeque<Coordinate> d = new MyDeque<Coordinate>();
 
-    public Frontier(){
-	queue = new Object[10];
-	head = 1;
-	tail = 0;
-	size = 0;
+    public Frontier(int t){
+	type = t;
     }
 
-    public void addFirst(T value){
-	head --;
-	if(head < 0){
-	    head = queue.length - 1; 
+    public void add(Coordinate value){
+	if (type == 0){
+	    d.addLast(value);
+	}else if(type == 1){
+	    d.addFirst(value);
 	}
-	if (size >= queue.length){
-	    head++;
-	    enlarge();
-	    this.addFirst(value);
-	    return;
-	}
-	queue[head] = value;
-	size ++;
     }
 
-    public void addLast(T value){
-	tail ++;
-	if(size >= queue.length){
-	    tail--;
-	    enlarge();
-	    this.addLast(value);
-	    return;
-	}
-	if(tail > queue.length - 1){
-	    tail = 0;
-	}
-	queue[tail] = value;
-	size ++;
+    public Coordinate remove(){
+	return d.removeFirst();
     }
-
-    public T removeFirst(){
-	if(size == 0){
-	    throw new NoSuchElementException();
-	}
-	T a = (T)queue[head];
-	queue[head] = null;
-	head ++;
-	if(head > queue.length){
-	    head = 0;
-	}
-	size --;
-	if (size < queue.length / 3){
-	    shrink();
-	}
-	return a;
-    }
-
-    public T removeLast(){
-    	if(size == 0){
-	    throw new NoSuchElementException();
-	}
-	T a = (T)queue[tail];
-	queue[tail] = null;
-	tail --;
-	if(tail < 0){
-	    tail = queue.length - 1;
-	}
-	size --;
-	if (size < queue.length / 3){
-	    shrink();
-	}
-	return a;
-    }
-
-    public T getFirst(){
-	if(size == 0){
-	    throw new NoSuchElementException();
-	}
-	return (T)queue[head];
-    }
-
-    public T getLast(){
-	if(size == 0){
-	    throw new NoSuchElementException();
-	}
-	return (T)queue[tail];
-    }
-
-    public void enlarge(){
-	Object[] copy = new Object[size * 2];
-	int j = 0;
-	for(int i = head; i != tail; i ++){
-	    if(i >= queue.length){
-		i = 0;
-	    }
-	    copy[j] = queue[i];
-	    j++;
-	}
-	copy[j] = queue[tail];
-	head = 0;
-	tail = j;
-	queue = copy;
-    }
-
-    public void shrink(){
-	Object[] copy = new Object[queue.length / 2];
-	int j = 0;
-	for(int i = head; i != tail; i ++){
-	    if(i >= queue.length){
-		i = 0;
-	    }
-	    copy[j] = queue[i];
-	    j++;
-	}
-	copy[j] = queue[tail];
-	head = 0;
-	tail = j;
-	queue = copy;
-    }
-
+   
     public int size(){
-	return size;
-    }
-
-    public int length(){
-	return queue.length;
+	return d.size;
     }
 
     public String toString(){
-	if(queue.length == 0){
-	    return "[]";
-	}
-	String a = "[" + queue[head];
-	for(int i = head + 1; i != tail + 1; i ++){
-	    if(i >= queue.length){
-		i = 0;
-	    }
-	    a += ", " + queue[i];
-	}
-	a += "]";
-	return a;
+	return d.toString();
     }
 }
--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.io.*;
 
 public class Maze{
     public char[][] maze;
     public Coordinate start, end;
-    public int size;
+    public int maxX, maxY, startX, startY;
+    public boolean solvable = true;
     
 
     private static final String clear =  "\033[2J";
@@ -14,75 +16,73 @@ public class Maze{
 	return ("\033[" + x + ";" + y + "H");
     }
 
+    public void wait(int millis){
+	try{
+	    Thread.sleep(millis);
+	}
+	catch(InterruptedException e){
+	}
+    }
+
     public String name(){
 	return "dickersin.nino";
     }
 
-    public Maze(){
-	maze = new char[][]{ {'S','.','*','*','.'},
-			     {'.','.','.','*','.'},
-			     {'.','*','.','*','.'},
-			     {'.','*','.','.','.'},
-			     {'.','.','*','.','E'} };
-	start = new Coordinate(0,0);
-	end = new Coordinate(5,5);
-	size = 5;
-	next.addFirst(start);
-    }
-    /**
     public Maze(String filename){
 	String a = "";
-	int r = 0;
-	int c = 0;
-	Scanner sc = new Scanner(new File (filename));
-	while(sc.hasNext()){
-	    String c = sc.next();
-	    if(c.equals("NEXT")){
-		r ++;
-		c = 0;
-	    }else{
-		a += c;
-		c ++;
+	startX = -1;
+	startY = -1;
+	try{
+	    Scanner sc = new Scanner(new File (filename));
+	    while(sc.hasNext()){
+		String b = sc.nextLine();
+		if(maxY == 0){
+		    maxX = b.length();
+		}
+		a += b;
+		maxY++;
 	    }
 	}
-	maze = new char[c][r];
-	for(int i = 0; i < maze.length; i ++){
-	    for(int j = 0; j < maze[0].length; j++){
-		maze[i][j] = a.substring(0, 1);
-		a = a.substring(1);
-	    }
+	catch(Exception e){
+	    System.out.println("File could not be read.");
+	    e.printStackTrace();
+	    System.exit(0);
 	}
-    }
-    **/
 
+	maze = new char[maxX][maxY];
+	for(int i = 0; i < a.length(); i ++){
+	    char c = a.charAt(i);
+	    maze[i % maxX][i / maxX] = c;
+	    if(c=='S'){
+		startX = i % maxX;
+		startY = i / maxX;
+	    }
+	}
+	start = new Coordinate(startX, startY);
+    }
+
+    
     public String toString(){
-	return this.toString(false);
+	String ans = "";
+	for(int i = 0; i < maxX * maxY; i++){
+	    if(i % maxX == 0 && i != 0){
+		ans += "\n";
+	    }
+	    ans += maze[i % maxX][i / maxX];
+	}
+	return ans;
     }
 
     public String toString(boolean animate){
-	String a = "";
-	return "banana";
+	if(animate){
+	    return clear + hide + go(0,0) + toString() + "\n" + show;
+	}else{
+	    return toString() + "\n";
+	}
     }
 
     public boolean solveBFS(boolean animate){
-	public Frontier<Coordinate> next = new Frontier<Coordinate>();
-	while(current != end){
-	    Coordinate current = next.getFirst();
-	    int x = current.getX();
-	    int y = current.getY();
-	    if(x - 1 > 0 && !(maze[y][x - 1]).equals('*')){
-	    
-	    }
-	    if(x + 1 < size && !(maze[y][x + 1]).equals('*')){
-
-	    }
-	    if(y - 1 > 0 && !(maze[y - 1][x]).equals('*')){
-
-	    } if(y + 1 < size && !(maze[y + 1][x]).equals('*')){
-
-	    }
-	}
-	return true;
+	return solve(animate, 0);
     }
 
     public boolean solveBFS(){
@@ -90,21 +90,92 @@ public class Maze{
     }
 
     public boolean solveDFS(boolean animate){
-	return true;
+	return solve(animate, 1);
     }
 
     public boolean solveDFS(){
 	return solveDFS(false);
     }
 
-    /**
-    public int[] solutionCoordinates(){
-	
+    public boolean solve(boolean animate, int type){
+	Frontier next = new Frontier(type);
+	boolean found = false;
+	next.add(start);
+	while(!found){
+	    if(next.size() == 0){
+		System.out.println( toString(animate) );
+		System.out.println("No solution");
+		return false;
+	    }else if(endFind(next, animate)){
+		found = true;
+	    }
+	}
+	if(found){
+	    Coordinate x = end.getLast();
+	    while(x.getDistance() > 0){
+		if(animate){
+		    System.out.println(toString(animate));
+		    wait(50);
+		}
+		maze[x.getRow()][x.getCol()] = '@';
+		x = x.getLast();
+	    }
+	    System.out.println(toString(animate));
+	    return true;
+	}
+	return false;
     }
-    **/
+
+    public boolean endFind(Frontier a, boolean animate){
+	if(animate){
+	    System.out.println(toString(animate));
+	    wait(70);
+	}
+	Coordinate y = a.remove();
+	if(maze[y.getRow()][y.getCol()] == 'E'){
+	    end = y;
+	    return true;
+	}
+	if (maze[y.getRow()][y.getCol()] == ' ' || maze[y.getRow()][y.getCol()] == 'S'){
+	    if ( maze[y.getRow()][y.getCol()] == ' ' ){
+		maze[y.getRow()][y.getCol()] = 'x';
+	    }
+	    if ( maze[y.getRow()][y.getCol()+1] == ' ' || maze[y.getRow()][y.getCol()+1] == 'E' ){
+		a.add(new Coordinate(y.getRow(), y.getCol()+1, y.getDistance()+1, y)); 
+	    }
+	    if ( maze[y.getRow()][y.getCol()-1] == ' ' || maze[y.getRow()][y.getCol()-1] == 'E'){
+		a.add(new Coordinate(y.getRow(), y.getCol()-1, y.getDistance()+1, y)); 
+	    }
+	    if ( maze[y.getRow()+1][y.getCol()] == ' ' || maze[y.getRow()+1][y.getCol()] == 'E' ){
+		a.add(new Coordinate(y.getRow()+1, y.getCol(), y.getDistance()+1, y));
+	    }
+	    if ( maze[y.getRow()-1][y.getCol()] == ' ' || maze[y.getRow()-1][y.getCol()] == 'E'){
+		a.add(new Coordinate(y.getRow()-1, y.getCol(), y.getDistance()+1, y));
+	    }
+	}
+	return false;
+    }
+
+    public int[] solutionCoordinates(){
+	if(end == null){
+	    return new int[0];
+	}
+	int[] a = new int[(end.getDistance() + 1) * 2];
+	Coordinate current = end;
+	for (int i = (end.getDistance() + 1) * 2 - 1; i >= 0; i -= 2){
+	    a[i] = current.getCol();
+	    a[i - 1] = current.getRow();
+	    current = current.getLast();
+	}
+	return a;
+    }
 
     public static void main(String[]args){
-	Maze a = new Maze();
-	//	Maze b = new Maze("MazeFile.txt");
+	Maze a = new Maze("MazeFile.txt");
+	a.solveBFS(true);
+	//	a.solveBFS(false);
+	//	a.solveDFS(true);
+	//	a.solveDFS(false);
+	System.out.println(Arrays.toString(a.solutionCoordinates()));
     }
 }
