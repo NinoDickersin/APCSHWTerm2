c0863c1854803a9e873e57c0aedf612b1e110c24
Nino Dickersin <n.dickersin@gmail.com>
2015-05-12 13:07:14 -0400

    add method + enlarge and swap helper functions

--- 13MyHeap/myHeap.java
+++ 13MyHeap/myHeap.java
@@ -5,15 +5,53 @@ public class MyHeap{
     public MyHeap(){
 	isMax = true;
 	heap = new int[10];
+	heap[0] = 0;
     }
 
     public MyHeap(boolean isMax){
 	this.isMax = isMax;
 	heap = new int[10];
+	heap[0] = 0;
     }
 
-    public void add(int){
+    public void add(int x){
+	boolean notOrdered = true;
+	heap[0] ++;
+	if(heap[0] >= heap.length()){
+	    this.enlarge();
+	}
+	heap[heap[0]] = x;
+	int index = heap[0];
+	while(notOrdered){
+	    if(isMax){
+		if(heap[index] > heap[index/2]){
+		    this.swap(index, index/2);
+		}else{
+		    notOrdered = false;
+		}
+	    }else{
+		if(heap[index] < heap[index/2]){
+		    this.swap(index, index/2);
+		}else{
+		    notOrdered = false;
+		}
+	    }
+	}
+    }
 
+    public void enlarge(){
+	int[] copy = new int[heap[0] * 2];
+	copy[0] = heap[0];
+	for(int i = 1; i < heap.length(); i ++){
+	    copy[i] = heap[i];
+	}
+	heap = copy;
+    }
+
+    public void swap(int a, int b){
+	int temp = heap[a];
+	heap[a] = heap[b];
+	heap[b] = temp;
     }
 
     public int peek(){
