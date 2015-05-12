7a1dacdd075f3eff3cb78e94f5fe84e528c30aee
Nino Dickersin <n.dickersin@gmail.com>
2015-05-12 12:52:55 -0400

    constructors + framework

---
+++ 13MyHeap/myHeap.java
@@ -0,0 +1,34 @@
+public class MyHeap{
+    boolean isMax;
+    int[] heap;
+
+    public MyHeap(){
+	isMax = true;
+	heap = new int[10];
+    }
+
+    public MyHeap(boolean isMax){
+	this.isMax = isMax;
+	heap = new int[10];
+    }
+
+    public void add(int){
+
+    }
+
+    public int peek(){
+
+    }
+
+    public int remove(){
+
+    }
+
+    public String toString(){
+
+    }
+
+    public static void main (String[]args){
+
+    }
+}
